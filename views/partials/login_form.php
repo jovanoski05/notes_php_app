d27cf4c09bcec187171e79b9488ecc1515c7bba0
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-11 22:52:32 +0200

    Cleaned the login form, removed the email field

--- views/partials/login_form.php
+++ views/partials/login_form.php
@@ -1,17 +1,13 @@
 <div class="d-flex justify-content-center text-center">
 <form class="justify-content-center">
   <div class="form-group mb-3">
-    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" placeholder="name@email.com" required>
-  </div>
-  <div class="form-group mb-3">
     <label for="username">Username</label>
-    <input type="text" class="form-control" id="username" placeholder="username" required>
+    <input type="text" class="form-control" id="username" placeholder="Username" required>
   </div>
   <div class="form-group mb-3">
     <label for="password">Password</label>
     <input type="password" class="form-control" id="password" placeholder="Password" required>
   </div>
-  <input type="submit" class="btn btn-outline-warning   ">
+  <input type="submit" class="btn btn-outline-warning" value="Log In">
 </form>
 </div>
