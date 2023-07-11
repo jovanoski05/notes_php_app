edd53c43e4eb1a622060e28bad1e0946a0456e5c
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-11 22:49:08 +0200

    Added some features to the login form

--- views/login.view.php
+++ views/login.view.php
@@ -4,5 +4,5 @@ require "partials/header.php";
 require "partials/navigation.php";
 $header="Log In";
 require "partials/title.php";
-
+require "partials/login_form.php";
 require "partials/footer.php";
---
+++ views/partials/login_form.php
@@ -0,0 +1,17 @@
+<div class="d-flex justify-content-center text-center">
+<form class="justify-content-center">
+  <div class="form-group mb-3">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" placeholder="name@email.com" required>
+  </div>
+  <div class="form-group mb-3">
+    <label for="username">Username</label>
+    <input type="text" class="form-control" id="username" placeholder="username" required>
+  </div>
+  <div class="form-group mb-3">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Password" required>
+  </div>
+  <input type="submit" class="btn btn-outline-warning   ">
+</form>
+</div>
