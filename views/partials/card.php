1ef978850d93c226d82976811d3c8ea7910fe0b0
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-11 10:19:02 +0200

    Adding shadows to the cards

--- views/partials/card.php
+++ views/partials/card.php
@@ -1,4 +1,4 @@
-<div class="card text-white bg-warning mb-3" style="max-width: 18rem; margin-left:10px; margin-right:10px;">
+<div class="card text-white bg-warning mb-3 shadow" style="max-width: 18rem; margin-left:10px; margin-right:10px;">
   <div class="card-header"><?= $card['date'] ?></div>
   <div class="card-body">
     <h5 class="card-title"><?= $card['title'] ?></h5>
