17262c05de3f9db00399fc870c8d58699e93fb77
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-12 09:31:30 +0200

    Added the form elements to signup_form.php

---
+++ views/partials/signup_form.php
@@ -0,0 +1,21 @@
+<div class="d-flex justify-content-center text-center">
+<form class="justify-content-center">
+<div class="form-group mb-3">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="name@email.com" required>
+  </div>
+  <div class="form-group mb-3">
+    <label for="username">Username</label>
+    <input type="text" class="form-control" id="username" placeholder="Username" required>
+  </div>
+  <div class="form-group mb-3">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Password" required>
+  </div>
+  <div class="form-group mb-3">
+    <label for="conf_password">Confirm Password</label>
+    <input type="password" class="form-control" id="conf_password" placeholder="Confirm Password" required>
+  </div>
+  <input type="submit" class="btn btn-outline-warning" value="Sign Up">
+</form>
+</div>
