66d3321386c6a2d1668cab2132ebea3afed1ff86
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-12 10:08:45 +0200

    Made the default fetch mode to be assoc

--- Database.php
+++ Database.php
@@ -7,7 +7,9 @@ class Database{
     {                                                                       //You can edit the configuration in config.php file
         $dsn = "mysql:". http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn, $user, $password);
+        $this->connection = new PDO($dsn, $user, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query, $params=[]){      //$query is the query that will be executed
