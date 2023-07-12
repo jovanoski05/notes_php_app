93459549ab0eacfe182b5c1e3e4d928fe0305009
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-12 11:32:39 +0200

    Added custom function to check if the user is authorized and called it in the controller/note.php

--- controller/note.php
+++ controller/note.php
@@ -4,11 +4,13 @@ $db=new Database($config['database']);
 
 $card=$db->query("SELECT * FROM notes WHERE id = :id", ['id' => $_GET['id']])->findOrFail();
 
-$current_user_id=1;
+$current_user_id = 1;
 
-if ($card['user_id']!=$current_user_id){
+/*if ($card['user_id']!=$current_user_id){
     abort(Response::FORBIDDEN);
-}
+}*/
+
+authorize($card['user_id']==$current_user_id);
 
 require "views/note.view.php";
 
--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ function dd($var){
     echo "</pre>";
     die();
 }
+
+function authorize($condition, $code=Response::FORBIDDEN){
+    if (!$condition){
+        Abort($code);
+    }
+}
