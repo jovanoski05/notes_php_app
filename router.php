2c6bbbf04ab67262466728e50181c90bcfb8d5e2
Ivan Jovanoski <jovanoski.ivan12345@gmail.com>
2023-07-12 11:44:56 +0200

    Created routes php file that contains all of the routes and required it in the router

--- router.php
+++ router.php
@@ -4,12 +4,7 @@ $url = parse_url($_SERVER["REQUEST_URI"]);
 $url = $url["path"];
 $config = require "config.php";
 
-$valid_uris =[
-    "/notesapp/" => "controller/index.php",
-    "/notesapp/create_notes" => "controller/create_notes.php",
-    "/notesapp/login" => "controller/login.php",
-    "/notesapp/note" => "controller/note.php"
-];
+$valid_uris = require "routes.php";
 
 function routeToUrl($url, $valid_uris, $config){      // adding $result_query in oreder to get the fetched results in the targeted page
     if (array_key_exists($url, $valid_uris)){
